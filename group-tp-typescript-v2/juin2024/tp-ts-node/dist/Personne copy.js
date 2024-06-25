1e24e24a57271aa85197525acb375f6c7d062a9e
didier <d2f.defrance@gmail.com>
2024-06-25 09:34:33 +0200

    s

--- group-tp-typescript-v2/juin2024/tp-ts-node/dist/Personne copy.js
+++ group-tp-typescript-v2/juin2024/tp-ts-node/dist/Personne copy.js
@@ -1,12 +1,22 @@
 "use strict";
 class Personne {
+    get age() {
+        return this._age;
+    }
+    set age(age) {
+        if (age >= 0)
+            this._age = age;
+        //else throw "age négatif invalide"; //à ratrapper via try/catch
+        else
+            console.log("age négatif invalide");
+    }
     constructor(numero = 0, nom = "?", age = 0) {
         this.numero = numero;
         this.nom = nom;
-        this.age = age;
+        this._age = age;
     }
     incrementAge() {
-        this.age++;
+        this._age++;
     }
 }
 let p1 = new Personne(1, "toto", 50);
@@ -16,5 +26,7 @@ let p2 = new Personne();
 console.log("p2=" + JSON.stringify(p2));
 p2.age = 40;
 p2.incrementAge();
+console.log("p2=" + JSON.stringify(p2));
+p2.age = -5; //sera refusé , pas pris en compte
 console.log("p2=" + JSON.stringify(p2));
 //# sourceMappingURL=Personne%20copy.js.map
