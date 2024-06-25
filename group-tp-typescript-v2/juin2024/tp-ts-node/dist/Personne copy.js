7c7d02ad8b5d2447bda271494be90a4ab9d6c69c
didier <d2f.defrance@gmail.com>
2024-06-25 11:14:43 +0200

    s

--- group-tp-typescript-v2/juin2024/tp-ts-node/dist/Personne copy.js
+++ group-tp-typescript-v2/juin2024/tp-ts-node/dist/Personne copy.js
@@ -1,28 +1,14 @@
 "use strict";
 class Personne {
-    /*
-    private  static _ageMajorite : number = 18;
-
-    public static get ageMajorite(){
-        return Personne._ageMajorite;
-    }
-
-    public static set ageMajorite(ageMajorite:number){
-        Personne._ageMajorite=ageMajorite;
-    }
-    */
     constructor(numero = 0, nom = "?", _age = 0) {
         this.numero = numero;
         this.nom = nom;
         this._age = _age;
     }
+    occupations() {
+        return "manger,dormir,loisir";
+    }
     estMajeur() {
-        /*
-        if(this._age >= Personne.ageMajorite)
-            return true;
-        else
-        return false;
-        */
         return (this._age >= Personne.ageMajorite);
     }
     get age() {
@@ -39,8 +25,26 @@ class Personne {
         this._age++;
     }
 }
-//public numTelephone : string ="0606050606";
 Personne.ageMajorite = 18;
+class Employe extends Personne {
+    constructor(numero, nom, age, salaire) {
+        super(numero, nom, age);
+        this.salaire = 0;
+        this.salaire = salaire;
+    }
+    occupations() {
+        return super.occupations() + ",travailler";
+    }
+    augmenterSalaire(pct) {
+        this.salaire = this.salaire * (1 + pct / 100);
+    }
+}
+var e1 = new Employe(1, "toto", 35, 2500);
+console.log("Empoye e1 :" + JSON.stringify(e1));
+//e1.salaire=3000;
+e1.augmenterSalaire(2); //2% augmentation
+console.log("Empoye e1 :" + JSON.stringify(e1));
+console.log("e1.occupations()=" + e1.occupations());
 let p1 = new Personne(1, "toto", 50);
 console.log("p1=" + JSON.stringify(p1));
 console.log(`p1.age=${p1.age}`);
@@ -55,6 +59,6 @@ console.log("p2 estMajeur ? : " + p2.estMajeur());
 p2.age = 17;
 console.log("p2=" + JSON.stringify(p2));
 console.log("p2 estMajeur ? : " + p2.estMajeur());
-Personne.ageMajorite = 17;
-console.log("p2 estMajeur ? : " + p2.estMajeur());
+//Personne.ageMajorite=17;
+//console.log("p2 estMajeur ? : " + p2.estMajeur());
 //# sourceMappingURL=Personne%20copy.js.map
