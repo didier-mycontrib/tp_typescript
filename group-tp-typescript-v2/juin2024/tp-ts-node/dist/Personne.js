7892e87b713c12034002c6d8ca1a13de0f578fae
didier <d2f.defrance@gmail.com>
2024-06-25 10:23:35 +0200

    s

--- group-tp-typescript-v2/juin2024/tp-ts-node/dist/Personne.js
+++ group-tp-typescript-v2/juin2024/tp-ts-node/dist/Personne.js
@@ -1,9 +1,29 @@
 "use strict";
 class Personne {
+    /*
+    private  static _ageMajorite : number = 18;
+
+    public static get ageMajorite(){
+        return Personne._ageMajorite;
+    }
+
+    public static set ageMajorite(ageMajorite:number){
+        Personne._ageMajorite=ageMajorite;
+    }
+    */
     constructor(numero = 0, nom = "?", _age = 0) {
         this.numero = numero;
         this.nom = nom;
         this._age = _age;
+    }
+    estMajeur() {
+        /*
+        if(this._age >= Personne.ageMajorite)
+            return true;
+        else
+        return false;
+        */
+        return (this._age >= Personne.ageMajorite);
     }
     get age() {
         return this._age;
@@ -19,6 +39,8 @@ class Personne {
         this._age++;
     }
 }
+//public numTelephone : string ="0606050606";
+Personne.ageMajorite = 18;
 let p1 = new Personne(1, "toto", 50);
 console.log("p1=" + JSON.stringify(p1));
 console.log(`p1.age=${p1.age}`);
@@ -29,4 +51,10 @@ p2.incrementAge();
 console.log("p2=" + JSON.stringify(p2));
 p2.age = -5; //sera refusé , pas pris en compte
 console.log("p2=" + JSON.stringify(p2));
+console.log("p2 estMajeur ? : " + p2.estMajeur());
+p2.age = 17;
+console.log("p2=" + JSON.stringify(p2));
+console.log("p2 estMajeur ? : " + p2.estMajeur());
+Personne.ageMajorite = 17;
+console.log("p2 estMajeur ? : " + p2.estMajeur());
 //# sourceMappingURL=Personne.js.map
