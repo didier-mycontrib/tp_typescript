a60f73e3cac3b27850622d5274d12fa87e61ee67
didier <d2f.defrance@gmail.com>
2024-06-26 12:33:43 +0200

    s

--- group-tp-typescript-v2/juin2024/tp-ts-node/dist/appelWs.js
+++ group-tp-typescript-v2/juin2024/tp-ts-node/dist/appelWs.js
@@ -3,10 +3,16 @@
 npm install -s node-fetch
 npm i --save-dev @types/node-fetch
 */
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const node_fetch_1 = __importDefault(require("node-fetch"));
 async function appelWs() {
     try {
         const url = "https://catfact.ninja/fact";
-        let response = await fetch(url);
+        let response;
+        response = await (0, node_fetch_1.default)(url);
         let res = await response.json();
         console.log("res=" + JSON.stringify(res));
     }
--- group-tp-typescript-v2/juin2024/tp-ts-node/src/appelWs.ts
+++ group-tp-typescript-v2/juin2024/tp-ts-node/src/appelWs.ts
@@ -3,10 +3,13 @@ npm install -s node-fetch
 npm i --save-dev @types/node-fetch
 */
 
+import fetch , { Response} from "node-fetch";
+
 async function appelWs(){
     try{
         const url ="https://catfact.ninja/fact";
-        let response = await fetch(url);
+        let response  : Response;
+        response= await fetch(url);
         let res = await response.json();
         console.log("res="+ JSON.stringify(res));
     }catch(e){
